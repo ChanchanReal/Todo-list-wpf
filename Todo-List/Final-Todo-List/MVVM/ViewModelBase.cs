54a6ec04ca60d96f9727273c721f8c9b1d494db1
Crix_ <92776805+ChanchanReal@users.noreply.github.com>
2024-02-12 13:33:40 +0800

    Added mvvm

---
+++ Todo-List/Final-Todo-List/MVVM/ViewModelBase.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Final_Todo_List.MVVM
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        public void OnPropertyChange([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        public Action<object> OnExecute;
+        public Func<object, bool> _CanExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action<object> _execute, Func<object, bool> _canexecute = null)
+        {
+            OnExecute = _execute;
+            _CanExecute = _canexecute;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return _CanExecute == null || _CanExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            OnExecute(parameter);
+        }
+    }
+}
