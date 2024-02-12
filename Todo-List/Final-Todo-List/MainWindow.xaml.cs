05bf54fb2fc41e9b9275aae2595f64921ec242ff
Crix_ <92776805+ChanchanReal@users.noreply.github.com>
2024-02-12 13:30:54 +0800

    add items

---
+++ Todo-List/Final-Todo-List/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Final_Todo_List.ViewModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Final_Todo_List
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            MainWindowViewModel vm = new MainWindowViewModel();
+            DataContext = vm;
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else WindowState = WindowState.Normal;
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
