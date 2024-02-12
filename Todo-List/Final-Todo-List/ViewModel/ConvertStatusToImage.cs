21bd408fa3c9fd662ba1c4ea884f00c673eb1d69
Crix_ <92776805+ChanchanReal@users.noreply.github.com>
2024-02-12 13:31:36 +0800

    Add files via upload

---
+++ Todo-List/Final-Todo-List/ViewModel/ConvertStatusToImage.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Final_Todo_List.ViewModel
+{
+    class ConvertStatusToImage : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string item)
+            {
+                string assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
+                string imagePath = item switch
+                {
+                    "Pending" => $"pack://application:,,,/{assemblyName};component/Images/hourglass.png",
+                    "Waiting" => $"pack://application:,,,/{assemblyName};component/Images/pause.png",
+                    "Complete" => $"pack://application:,,,/{assemblyName};component/Images/done.png",
+                    "Canceled" => $"pack://application:,,,/{assemblyName};component/Images/cancel.png"
+                };
+
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.UriSource = new Uri(imagePath, UriKind.RelativeOrAbsolute);
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.EndInit();
+                return image;
+            }       
+            
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Todo-List/Final-Todo-List/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,124 @@
+﻿using Final_Todo_List.Model;
+using Final_Todo_List.MVVM;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Windows;
+namespace Final_Todo_List.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private string _todoList;
+        private bool txtPlaceHolder = false;
+        private DataManagerHandler _dataHandler;
+        public ObservableCollection<Todo> TodoListCollection { get; set; }
+        public RelayCommand DeleteItem { get; set; }
+        public RelayCommand AddTodo => new RelayCommand(_execute => AddItemTODO());
+        public RelayCommand SaveBeforeClose => new RelayCommand(_execute => SaveOnClose());
+        public RelayCommand UpdateStatus { get; set; }
+        public MainWindowViewModel()
+        {
+            _dataHandler = new DataManagerHandler();
+
+            TodoListCollection = new ObservableCollection<Todo>(_dataHandler.Read());
+
+            UpdateStatus = new RelayCommand(ChangeStatus);
+            DeleteItem = new RelayCommand(DeleteSelectedItem);
+        }
+        private Todo selectedItem;
+
+        public Todo SelectedItem
+        {
+            get { return selectedItem; }
+            set { selectedItem = value; OnPropertyChange(); }
+        }
+
+
+        public string TodoList
+        {
+            get { return _todoList; }
+            set 
+            { 
+                _todoList = value; OnPropertyChange();
+                if (!string.IsNullOrEmpty(TodoList))
+                {
+                    TxtPlaceHolder = true;
+                }
+                else
+                {
+                    TxtPlaceHolder = false;
+                }
+            }
+        }
+        public bool TxtPlaceHolder
+        {
+            get { return txtPlaceHolder; }
+            set { txtPlaceHolder = value; OnPropertyChange(); }
+        }
+        public void DeleteSelectedItem(object todo)
+        {
+            if(todo is Todo)
+            {
+                Todo itemSelected = (Todo)todo;
+                TodoListCollection.Remove(itemSelected);
+            }
+        }
+
+        public void SaveOnClose()
+        {
+            Task task = SaveClose();
+            task.Wait();
+        }
+        public void Save()
+        {
+            _dataHandler.Write(TodoListCollection.ToList());
+        }
+        public Task SaveClose()
+        {
+            return Task.Run(() =>
+            {
+                Save();
+            });
+        }
+
+        public void AddItemTODO()
+        {
+            IEnumerable<string> item = from o in TodoListCollection
+                                       where o.TodoList == TodoList 
+                                       select o.TodoList;
+
+            if (!item.Contains<string>(TodoList))
+            {
+                if (!string.IsNullOrEmpty(TodoList))
+                {
+                    TodoListCollection.Add(new Todo(TodoList));
+
+                    _dataHandler.Write(TodoListCollection.ToList());
+                }
+                
+            }
+            else
+            {
+                    MessageBox.Show(TodoList, "Already exist!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+           
+        }
+
+        public void ChangeStatus(object selectedItem)
+        {
+            if (selectedItem is Todo todo)
+            {
+                todo.Status = todo.Status switch
+                {
+                    "Pending" => "Waiting",
+                    "Waiting" => "Complete",
+                    "Complete" => "Canceled",
+                    "Canceled" => "Pending",
+                    _ => throw new NotImplementedException()
+                };
+            }
+        }
+        public enum StatusTodo { Complete, Waiting, Canceled, Pending }
+    }
+}
+
+
